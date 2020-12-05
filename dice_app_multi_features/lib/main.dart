0556dac322846f738a40867b6536cbeed857ed86
bilalahmad9 <bilalferoze99@gmail.com>
2020-12-05 12:08:46 +0500

    Add Roll Dice Button

--- dice_app_multi_features/lib/main.dart
+++ dice_app_multi_features/lib/main.dart
@@ -1,55 +1,77 @@
-import 'package:flutter/material.dart';
 import 'dart:math';
 
-void main() {
-  return runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
-        ),
-        body: DicePage(),
-      ),
-    ),
-  );
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+// This widget is the root of your application.
+@override
+Widget build(BuildContext context) {
+return MaterialApp(
+title: 'Flutter Demo',
+theme: ThemeData(
+primarySwatch: Colors.blue,
+),
+home: MyHomePage(),
+);
+}
 }
 
-class DicePage extends StatefulWidget {
-  @override
-  _DicePageState createState() => _DicePageState();
+class MyHomePage extends StatefulWidget {
+MyHomePage();
+
+@override
+_MyHomePageState createState() => _MyHomePageState();
 }
 
-class _DicePageState extends State<DicePage> {
-  var left_image = Random().nextInt(5);
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  left_image = 5;
-                });
+class _MyHomePageState extends State<MyHomePage> {
 
-                print('Value of Variable $left_image');
-              },
-              child: Image.asset('images/dice$left_image.png'),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Dice 02');
-              },
-            
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+var imageArray = ['dice1.png', 'dice2.png', 'dice3.png', 'dice4.png', 'dice5.png', 'dice6.png'];
+//var random = new Random();
+int randomIntForDiceOne = Random().nextInt(6);
+int randomIntForDiceTwo = Random().nextInt(6);
+
+@override
+Widget build(BuildContext context) {
+
+return Scaffold(
+backgroundColor: Colors.deepOrangeAccent,
+body: Padding(
+padding: const EdgeInsets.fromLTRB(21, 50, 21, 50),
+child: Column(
+children: <Widget>[
+Padding(
+padding: const EdgeInsets.only(bottom: 60),
+child: Text('The sum is: '+ (randomIntForDiceOne + randomIntForDiceTwo + 2).toString(),
+style: TextStyle(
+fontSize: 20,
+),
+),
+),
+Row(
+mainAxisAlignment: MainAxisAlignment.spaceAround,
+children: <Widget>[
+Image.asset('images/'+imageArray[randomIntForDiceOne], height: 150, width: 150,),
+Image.asset('images/'+imageArray[randomIntForDiceTwo], height: 150, width: 150,),
+],
+),
+Padding(
+padding: const EdgeInsets.only(top: 60),
+child: RaisedButton(onPressed: changeImage,
+child: Text('Roll Dice'),
+),
+)
+],
+),
+)
+);
+}
+
+void changeImage() {
+setState(() {
+randomIntForDiceOne = Random().nextInt(6);
+randomIntForDiceTwo = Random().nextInt(6);
+});
+}
 }
